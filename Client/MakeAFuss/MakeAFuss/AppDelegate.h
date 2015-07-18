348acd90c2bef2d99e18f468586c3d2e277a94c5
Andrei Balasanu <andrei.balasanu@softvision.ro>
2015-07-18 12:29:45 +0300

    Added Initial iOS Project structure

---
+++ Client/MakeAFuss/MakeAFuss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MakeAFuss
+//
+//  Created by Andrei Balasanu on 7/18/15.
+//  Copyright (c) 2015 Softvision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
