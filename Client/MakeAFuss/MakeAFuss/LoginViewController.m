3d0ee43055d7ac390a1592102c3a64a1a7b90f12
Andrei Balasanu <andrei.balasanu@softvision.ro>
2015-07-18 14:43:41 +0300

    Added authentication screen logic

--- Client/MakeAFuss/MakeAFuss/LoginViewController.m
+++ Client/MakeAFuss/MakeAFuss/LoginViewController.m
@@ -8,6 +8,55 @@
 
 #import "LoginViewController.h"
 
+static NSString * const ShowDashboardSegueIdentifier = @"ShowDashboard";
+
+@interface LoginViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+
+
+@end
+
 @implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self updateLoginButtonState];
+}
+
+
+#pragma mark - IBActions
+
+- (IBAction)textFieldDidChangeText:(id)sender {
+    [self updateLoginButtonState];
+    
+}
+
+- (IBAction)loginButtonTapped:(id)sender {
+    __weak typeof(self) weakSelf = self;
+    [self login:^(BOOL successful) {
+        [weakSelf showDashboard];
+    }];
+}
+
+#pragma mark - Private
+
+- (void)updateLoginButtonState {
+    BOOL loginEnabled = self.nameField.text.length > 0 && self.passwordField.text.length > 0;
+    self.loginButton.enabled = loginEnabled;
+}
+
+- (void)login:( void (^) (BOOL successful))completion {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (completion) completion(YES);
+    });
+}
+
+- (void)showDashboard {
+    [self performSegueWithIdentifier:ShowDashboardSegueIdentifier sender:self];
+}
 
 @end
--- Client/MakeAFuss/MakeAFuss/RegisterViewController.m
+++ Client/MakeAFuss/MakeAFuss/RegisterViewController.m
@@ -8,6 +8,57 @@
 
 #import "RegisterViewController.h"
 
+static NSString * const ShowDashboardSegueIdentifier = @"ShowDashboard";
+
+@interface RegisterViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordField;
+@property (weak, nonatomic) IBOutlet UIButton *registerButton;
+
+@end
+
 @implementation RegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self updateRegisterButtonState];
+}
+
+#pragma mark - IBActions
+
+- (IBAction)textFieldDidChangeText:(id)sender {
+    [self updateRegisterButtonState];
+
+}
+
+- (IBAction)registerButtonTapped:(id)sender {
+    __weak typeof(self) weakSelf = self;
+    [self registerUser:^(BOOL successful) {
+        [weakSelf showDashboard];
+    }];
+    
+}
+
+#pragma mark - Private
+
+- (void)updateRegisterButtonState {
+    BOOL hasValidLength = self.nameField.text.length > 0 && self.passwordField.text.length > 0;
+    BOOL isValidPassword = [self.passwordField.text isEqualToString:self.confirmPasswordField.text];
+    
+    self.registerButton.enabled = hasValidLength && isValidPassword;
+}
+
+- (void)registerUser:( void (^) (BOOL successful))completion {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (completion) completion(YES);
+    });
+}
+
+- (void)showDashboard {
+    [self performSegueWithIdentifier:ShowDashboardSegueIdentifier sender:self];
+}
 
 @end
