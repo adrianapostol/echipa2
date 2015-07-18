8029c441d257d3b96e31e30bbd061d5dd6e8c1be
Andrei Balasanu <andrei.balasanu@softvision.ro>
2015-07-18 15:21:28 +0300

    Implemented Categories Screen logic

--- Client/MakeAFuss/MakeAFuss/CategoriesViewController.m
+++ Client/MakeAFuss/MakeAFuss/CategoriesViewController.m
@@ -8,6 +8,80 @@
 
 #import "CategoriesViewController.h"
 
-@implementation CategoriesViewController
+@interface CategoriesTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
 @end
+
+@interface CategoriesViewController ()
+
+@property (nonatomic, strong) NSArray *categories;
+
+@end
+
+
+
+@implementation CategoriesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    __weak typeof(self) weakSelf = self;
+    [self loadCategories:^(NSArray *categories) {
+        weakSelf.categories = categories;
+        [weakSelf.tableView reloadData];
+    }];
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.categories.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    CategoriesTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Category Cell" forIndexPath:indexPath];
+    cell.titleLabel.text = self.categories[indexPath.row];
+    
+    return cell;
+}
+
+
+#pragma mark - IBActions
+
+- (IBAction)cancelButtonTapped:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)doneButtonTapped:(id)sender {
+    NSIndexPath *selectedIndexPath = [self.tableView indexPathForSelectedRow];
+    if (selectedIndexPath && [self.delegate respondsToSelector:@selector(categoriesViewController:didSelectCategory:)]) {
+        [self.delegate categoriesViewController:self didSelectCategory:self.categories[selectedIndexPath.row]];
+    }
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Private
+
+- (void)loadCategories:(void (^)(NSArray *categories)) completion {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSArray *categories = @[ @"Management", @"Colleagues", @"Administrative", @"Other" ];
+        if (completion) completion(categories);
+    });
+}
+
+@end
+
+@implementation CategoriesTableViewCell
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    self.accessoryType = selected ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+}
+
+@end
+
+
